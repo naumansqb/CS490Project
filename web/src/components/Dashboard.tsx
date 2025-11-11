4ac94836adab837da9403c68631785fb8420767b
Jeremy De La Rosa <jeremyd26937@gmail.com>
2025-11-11 17:52:18 -0500

    restored job analyticss dashboard

--- web/src/components/Dashboard.tsx
+++ web/src/components/Dashboard.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import JobStatisticsDashboard from "./jobs/JobsAnalytics";
 import UpcomingDeadlines from "./jobs/UpComingDeadlines";
 
 export default function Dashboard() {
   return (
     <div>
       <UpcomingDeadlines />
+      <JobStatisticsDashboard />
     </div>
 
   );
